f10af14e02a964536683bcaefc9ba80b2e1613da
rishika7-hub <srishika970@gmail.com>
2020-10-01 00:10:02 +0530

    Added:number of times head and tail won

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
+declare -A dict1
+
+count1=0
+count2=0
+
 echo "Welcome to Flip Coin Simulation"
 
-
+for (( i=1;i<=20;i++ ))
+do
 num=$(( $RANDOM % 2 ))
 if [ $num -eq 0 ]
 then
 	echo "Head"
+	let count1++
 else
 	echo "Tail"
+	let count2++
 fi
+done
