62dde086e8948f15da13dcbeac8cdc38a0f8331f
rishika7-hub <srishika970@gmail.com>
2020-10-01 00:26:54 +0530

    Added: modification

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -1,24 +1,46 @@
 #!/bin/bash
 declare -A dict1
 
-count1=0
-count2=0
-
 echo "Welcome to Flip Coin Simulation"
 
-for (( i=1;i<=20;i++ ))
+count=0
+count1=0 
+while [ true ]
 do
 num=$(( $RANDOM % 2 ))
+echo $num
 if [ $num -eq 0 ]
 then
 	echo "Head"
-	let count1++
+	((count++))
 else
 	echo "Tail"
-	let count2++
+	((count1++))
+fi
+if [ $count -eq 21 ]
+then
+	echo "Head is more"
+	break
+elif [ $count1 -eq 21 ]
+then
+	echo "Tail is more"
+	break
+else
+	echo "Fail"
 fi
 done
+echo "Head Won : $count"
+echo "Tail Won : $count1"
 
-echo "Head Won : $count1"
-echo "Tail Won : $count2"
-
+if [ $count -lt $count1 ]
+then
+	res1=$(( $count1 -$count ))
+	echo "Tail is won by $res1 point"
+elif [ $count1 -lt $count ]]
+then
+	res2=$(( $count - $count1 ))
+	echo "Head is won by $res2 point"
+elif [ $count1 -eq $count ]
+then
+	echo "It is tie"
+fi
