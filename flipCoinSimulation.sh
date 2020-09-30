018c6dcc55854f3e6ca43d1eff5c00f255e54ca9
rishika7-hub <srishika970@gmail.com>
2020-10-01 00:40:21 +0530

    Added:extention

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -7,9 +7,9 @@ count=0
 count1=0 
 while [ true ]
 do
-num=$(( $RANDOM % 2 ))
-echo $num
-if [ $num -eq 0 ]
+num1=$(( $RANDOM % 2 ))
+echo $num1
+if [ $num1 -eq 0 ]
 then
 	echo "Head"
 	((count++))
@@ -17,30 +17,12 @@ else
 	echo "Tail"
 	((count1++))
 fi
-if [ $count -eq 21 ]
-then
-	echo "Head is more"
-	break
-elif [ $count1 -eq 21 ]
-then
-	echo "Tail is more"
-	break
+diff=$(( $count1 - $count ))
+if [ $diff -gt 2 ] || [ $diff -gt -2 ]
+then break
 else
-	echo "Fail"
-fi
+continue
+fi 
 done
-echo "Head Won : $count"
-echo "Tail Won : $count1"
 
-if [ $count -lt $count1 ]
-then
-	res1=$(( $count1 -$count ))
-	echo "Tail is won by $res1 point"
-elif [ $count1 -lt $count ]]
-then
-	res2=$(( $count - $count1 ))
-	echo "Head is won by $res2 point"
-elif [ $count1 -eq $count ]
-then
-	echo "It is tie"
-fi
+
